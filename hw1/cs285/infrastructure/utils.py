33cb24f50657df987bcc3fd27f0a5b7c422a1495
alexkoven <40674821+alexkoven@users.noreply.github.com>
2025-03-26 10:40:02 -0700

    added util functions

--- hw1/cs285/infrastructure/utils.py
+++ hw1/cs285/infrastructure/utils.py
@@ -32,16 +32,16 @@ def sample_trajectory(env, policy, max_path_length, render=False):
                 img = env.render(mode='single_rgb_array')
             image_obs.append(cv2.resize(img, dsize=(250, 250), interpolation=cv2.INTER_CUBIC))
     
-        # TODO use the most recent ob to decide what to do
-        ac = TODO # HINT: this is a numpy array
-        ac = ac[0]
+        # use the most recent ob to decide what to do
+        ac = policy(ptu.from_numpy(ob)).detach().cpu().numpy()  # Convert observation to tensor, get action from policy
+        ac = ac[0]  # Extract first element since policy outputs batched actions
 
-        # TODO: take that action and get reward and next ob
-        next_ob, rew, done, _ = TODO
+        # take that action and get reward and next ob
+        next_ob, rew, done, _ = env.step(ac)  # Standard gym environment step
         
-        # TODO rollout can end due to done, or due to max_path_length
+        # rollout can end due to done, or due to max_path_length
         steps += 1
-        rollout_done = TODO # HINT: this is either 0 or 1
+        rollout_done = 1 if done or steps >= max_path_length else 0  # End if environment is done or max steps reached
         
         # record result of taking that action
         obs.append(ob)
